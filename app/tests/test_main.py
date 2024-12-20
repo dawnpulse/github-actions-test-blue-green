0a870d050fd759139ad1ffd13ce082defc00e3a5
KnyshBohdan <knysh.bohdan0205@gmail.com>
2024-12-20 12:35:16 +0200

    Added initial commit

---
+++ app/tests/test_main.py
@@ -0,0 +1,12 @@
+import pytest
+from main import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert "Hello World!" in response.data.decode()
